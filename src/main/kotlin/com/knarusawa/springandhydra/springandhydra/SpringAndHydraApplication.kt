119030114832ba761118e75a39e7f20061106909
k-narusawa <narusawakohei@team-lab.com>
2024-01-19 11:13:55 +0900

    initial commit

---
+++ src/main/kotlin/com/knarusawa/springandhydra/springandhydra/SpringAndHydraApplication.kt
@@ -0,0 +1,11 @@
+package com.knarusawa.springandhydra.springandhydra
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAndHydraApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringAndHydraApplication>(*args)
+}
---
+++ src/test/kotlin/com/knarusawa/springandhydra/springandhydra/SpringAndHydraApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.knarusawa.springandhydra.springandhydra
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringAndHydraApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
